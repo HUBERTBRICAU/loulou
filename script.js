ca2ab0483f2625cf7c6dda9fe1f20d9e899b3d11
HUBERTBRICAU <hubertbricau@gmail.com>
2024-06-27 11:51:35 +0200

    2806

---
+++ script.js
@@ -0,0 +1,8 @@
+    function myFunction() {
+      document.getElementById("cheval").innerHTML = "Paard Bayart";
+      document.getElementById("lorem").innerHTML = "Il est très important pour le client de réussir. Il aime les ennuis faciles, quand il est stimulé par le plaisir de la raison, il atteint le plaisir de certaines dettes physiques. Les temps ne sont pas présents.";
+    }
+    function dada() {
+      document.getElementById("cheval").innerHTML = "Cheval Bayart";
+      document.getElementById("lorem").innerHTML = "Il est très important pour le client de réussir. Il aime les ennuis facil";
+    }
